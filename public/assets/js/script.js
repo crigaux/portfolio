5b9d117d7ccdc5d3e87668c9872b77e57ba524a9
crigaux <crigaux057@gmail.com>
2022-07-17 11:03:08 +0200

    Implémentation du TypeText JS

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,5 +1,27 @@
+// ######################################################
+// #####   Injection de class pour mode jour/nuit   ##### 
+// ######################################################
+
 const lightMode = document.getElementById('lightMode');
 
 lightMode.addEventListener('change', () => {
     document.body.classList.toggle('light');
 })
+
+// ######################################################
+// #####              Animation du h1               ##### 
+// ######################################################
+
+const nameAnim = document.querySelector('h1');
+
+new Typewriter(nameAnim, {
+    deleteSpeed: 20,
+    typeSpeed: 50
+})
+.pauseFor(300)
+.typeString('<span style="font-style: italic;">Développeur Web</span>')
+.pauseFor(1000)
+.deleteChars(15)
+.pauseFor(400)
+.typeString('<span style="font-style: italic;">Apprenti Dev Web</span>')
+.start()
